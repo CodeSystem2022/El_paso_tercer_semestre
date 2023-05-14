97e2e025ed4ccd4ec4cb1e23fd5bad81669ad82c
gcaballero99 <104869969+gcaballero99@users.noreply.github.com>
2023-05-14 01:33:47 -0300

    Delete JavaScript/Semana1/JavaScriptCiclos/TSLeccion01DoWhile directory

--- JavaScript/Semana1/JavaScriptCiclos/TSLeccion01DoWhile/CiclosDoWhile.js
+++
@@ -1,9 +0,0 @@
-// Se crea un programa con una variable iniciada en cero que ira aumentado, hasta que la condición lo permita.
-
-let contador = 0;
-
-do {
-  contador++;
-} while (contador < 5);
-
-console.log("Valor final: " + contador);
