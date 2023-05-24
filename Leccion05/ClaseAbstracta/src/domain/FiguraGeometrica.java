845aaca3491a9a77d93467533a4b95e8ea777cfe
ivan.rdo <ivanramirez.deo@gmail.com>
2023-05-24 11:22:43 -0300

    Se incluye carpeta Leccion05 y proyectos ConversionObjetos, ClaseObjetos y ClaseAbstracta

---
+++ Leccion05/ClaseAbstracta/src/domain/FiguraGeometrica.java
@@ -0,0 +1,34 @@
+
+package domain;
+
+/**
+ *
+ * @author Ivan
+ */
+public abstract class FiguraGeometrica {
+    protected String tipoFigura;
+    
+    protected FiguraGeometrica(String tipoFigura){
+        this.tipoFigura = tipoFigura;
+    }
+    //Metodo abstracto
+    public abstract void dibujar();
+    
+    //Agregamos get y set
+
+    public String getTipoFigura() {
+        return tipoFigura;
+    }
+
+    public void setTipoFigura(String tipoFigura) {
+        this.tipoFigura = tipoFigura;
+    }
+
+    @Override
+    public String toString() {
+        return "FiguraGeometrica{" + "tipoFigura=" + tipoFigura + '}';
+    }
+    
+    
+    
+}
