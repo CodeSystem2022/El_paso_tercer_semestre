e442976513493c6560030f1c4b2ec091315d093b
leonardofer097 <104870457+leonardofer097@users.noreply.github.com>
2023-06-24 19:59:40 -0300

    Commit semana 6- Segunda parte
    Crear método mostrarOrden y hacer pruebas

--- JavaScript/Semana 6/TSLeccion08/SistemaVentas.js
+++ JavaScript/Semana 6/TSLeccion08/SistemaVentas.js
@@ -44,31 +44,32 @@ class Orden{
     }
     agregarProducto(producto){
         if(this._productos.length < Orden.getMAX_PRODUCTOS()){
-            this._productos.push(producto);//tenemos dos tipos de xintaxis, primer xintaxis
-            //this._productos[this._contadorProductosAgregados++]=producto;//segunda xintaxis
+            this._productos.push(producto);//Tenemos dos tipos de sintaxis, primer sintaxis
+            //this._productos[this._contadorProductosAgregados++]=producto;//segunda sintaxis
         }
         else{
             console.log('No se pueden agregar mas productos')
         }
-    }//fin del metodos agregar productos
+    }//Fin del metodos agregar productos
 
     calcularTotal(){
         let totalVentas = 0;
         for(let producto of this._productos){
             totalVentas+=producto.precio;
-        }//fin ciclo for
+        }//Fin ciclo for
         return totalVentas;
-    }//fin metodo calcular total
+    }//Fin metodo calcular total
 
 
     mostrarOrden(){
         let productosOrden = ' ';
         for(let producto of this._productos){
             productosOrden+= '\n{ '+ producto.toString()+' }';
-        }//fin del ciclo for
+        }//Fin del ciclo for
     console.log(`Orden:${this._idOrden}, Total: $${this.calcularTotal()}, Productos: ${productosOrden}`);
-    }//fin metodo mostrarOrden
-}// fin de la clase orden
+    }//Fin metodo mostrarOrden
+    
+}// Fin de la clase orden
 
 
 
