dcb993749adcc6180cc557c0e0b289ed02a794bd
leonardofer097 <104870457+leonardofer097@users.noreply.github.com>
2023-07-04 20:49:28 -0300

    Java-Clase12-Video 6,7
    -Prueba de método alistar
    -Creación método por id -> find by id

--- Java/Semana 8/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ Java/Semana 8/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -45,6 +45,45 @@ public class EstudianteDAO {
         return estudiantes;
     }//Fin método listar
 
+    //Método por id -> find by id
+    public boolean buscarEstudiantePorId(Estudiante estudiante){
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection con = getConnection();
+        String sql = "SELECT * FROM estudiantes2022 WHERE idestudiantes2022=?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, estudiante.getIdEstudiante());
+            rs = ps.executeQuery();
+            if (rs.next()){
+                estudiante.setNombre(rs.getString("nombre"));
+                estudiante.setApellido(rs.getString("apellido"));
+                estudiante.setTelefono(rs.getString("telefono"));
+                estudiante.setEmail(rs.getString("email"));
+                return true; // Se encontro un registro
+            }//Fin if
+        } catch (Exception e){
+            System.out.println("Ocurrio un error al buscar estudiante: "+e.getMessage());
+        }//Fin catch
+        finally {
+            try {
+                con.close();
+            }catch (Exception e){
+                System.out.println("Ocurrio un error al cerrar la conexión "+e.getMessage());
+            }// Fin catch
+        }//Fin finally
+        return false;
+    }
+
+    public static void main(String[] args) {
+        var estudianteDao = new EstudianteDAO();
+        //Listar los estudiantes
+        System.out.println("Listado de estudiantes");
+        List<Estudiante> estudiantes = estudianteDao.listarEstudiantes();
+        estudiantes.forEach(System.out::println);//Funcion lambda para imprimir
+    }
+
+
 }
 
 
